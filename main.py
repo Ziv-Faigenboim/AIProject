c22b3ec51a60698647a353a0581d4eb023b21fc0
Ziv-Faigenboim <ziv.lehavim@gmail.com>
2024-07-02 16:47:22 +0300

    Commit main file
    added only main file

---
+++ main.py
@@ -0,0 +1,20 @@
+
+from flask import Flask,redirect,url_for,render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html",content="Testing",r=3)
+#return "Hello! this is the main page <h1>Hello<h1>"
+
+#@app.route("/<name>")
+#def user(name):
+ #   return f"hello {name}!"
+
+#@app.route("/admin")
+#def admin():
+ #   return redirect(url_for("user",name="Admin!"))
+
+if __name__ == "__main__":
+    app.run()
